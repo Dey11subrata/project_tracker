d02cc342d3f025762332275a789b9549d4778dd1
Dey11subrata <deysubrata2311@gmail.com>
2024-05-10 10:54:54 +0530

    Annotated with @Repository

--- src/main/java/com/project/springboot/project_tracker/repository/project_repository/sprint/SprintRepository.java
+++ src/main/java/com/project/springboot/project_tracker/repository/project_repository/sprint/SprintRepository.java
@@ -2,6 +2,8 @@ package com.project.springboot.project_tracker.repository.project_repository.spr
 
 import com.project.springboot.project_tracker.model.project.sprint.Sprint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SprintRepository extends JpaRepository<Sprint, Integer> {
 }
