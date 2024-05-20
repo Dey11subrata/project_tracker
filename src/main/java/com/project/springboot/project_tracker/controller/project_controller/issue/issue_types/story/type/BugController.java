afae6e151ead0af6b58be241ac5ffb6bd8a0fe80
Dey11subrata <deysubrata2311@gmail.com>
2024-05-20 11:47:11 +0530

    project mapped with Bug

--- src/main/java/com/project/springboot/project_tracker/controller/project_controller/issue/issue_types/story/type/BugController.java
+++ src/main/java/com/project/springboot/project_tracker/controller/project_controller/issue/issue_types/story/type/BugController.java
@@ -1,13 +1,16 @@
 package com.project.springboot.project_tracker.controller.project_controller.issue.issue_types.story.type;
 
 import com.project.springboot.project_tracker.dto.project_dto.issue.issue_type.story.type.BugDto;
+import com.project.springboot.project_tracker.model.project.Project;
 import com.project.springboot.project_tracker.model.project.issue.issue_types.story.type.Bug;
+import com.project.springboot.project_tracker.service.project_service.ProjectService;
 import com.project.springboot.project_tracker.service.project_service.issue_service.issue_type_service.story_service.type_service.bug_service.BugService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/bug")
@@ -15,16 +18,24 @@ public class BugController {
 
     private BugService bugService;
     private ModelMapper modelMapper;
+    private ProjectService projectService;
 
     @Autowired
-    public BugController(BugService bugService, ModelMapper modelMapper) {
+    public BugController(BugService bugService, ModelMapper modelMapper, ProjectService projectService) {
         this.bugService = bugService;
         this.modelMapper = modelMapper;
+        this.projectService = projectService;
     }
 
     @PostMapping("/create")
-    public Bug createBug(@RequestBody BugDto bugDto){
+    public Bug createBug(@RequestBody BugDto bugDto, @RequestParam(name = "project-id") int projectId){
         Bug bugToCreate = modelMapper.map(bugDto, Bug.class);
+        // find project
+        Optional<Project> project = projectService.findAProjectById(projectId);
+
+        if(project.isPresent()){
+            bugToCreate.setProject(project.get());
+        }
         return bugService.createBug(bugToCreate);
     }
 
--- src/main/java/com/project/springboot/project_tracker/controller/project_controller/issue/issue_types/story/type/SubTaskController.java
+++ src/main/java/com/project/springboot/project_tracker/controller/project_controller/issue/issue_types/story/type/SubTaskController.java
@@ -1,13 +1,16 @@
 package com.project.springboot.project_tracker.controller.project_controller.issue.issue_types.story.type;
 
 import com.project.springboot.project_tracker.dto.project_dto.issue.issue_type.story.type.SubTaskDto;
+import com.project.springboot.project_tracker.model.project.Project;
 import com.project.springboot.project_tracker.model.project.issue.issue_types.story.type.SubTask;
+import com.project.springboot.project_tracker.service.project_service.ProjectService;
 import com.project.springboot.project_tracker.service.project_service.issue_service.issue_type_service.story_service.type_service.subtask_service.SubTaskService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/sub_task")
@@ -19,14 +22,13 @@ public class SubTaskController {
     public SubTaskController(SubTaskService subTaskService, ModelMapper modelMapper) {
         this.subTaskService = subTaskService;
         this.modelMapper = modelMapper;
+
     }
 
     @PostMapping("/create")
     public SubTask createSubTask(@RequestBody SubTaskDto subTaskDto){
-
         SubTask subTaskToCreate = modelMapper.map(subTaskDto, SubTask.class);
-
-        return subTaskService.createSubTask(subTaskToCreate);
+           return subTaskService.createSubTask(subTaskToCreate);
     }
 
     @GetMapping("")
